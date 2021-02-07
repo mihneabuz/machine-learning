6fc8f391224b310fd442ee45f44ccc8b6ad78f6f
mihneabuz <mihneabuzatu88@gmail.com>
2021-02-07 16:19:44 +0200

    Added 2 new projects [WIP]

---
+++ Fashion_pytorch/fashion.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+
+traindatafile = "train-images-idx3-ubyte"
+trainlabelfile = "train-labels-idx1-ubyte"
+testdatafile = "t10k-images-idx3-ubyte"
+testlabelfile = "t10k-labels-idx1-ubyte"
+
+print("Loading data...")
+magic1 = np.fromfile(traindatafile, dtype=np.int32, count=1).byteswap().squeeze()
+assert(magic1 == 2051)
+dims = np.fromfile(traindatafile, dtype=np.int32, count=3, offset=4).byteswap()
+dims = [dims[0], dims[1] * dims[2]]
+X_train = np.memmap(traindatafile, dtype=np.dtype('>u1'), mode='r', offset=16).reshape(dims).T
+print("Train set:", X_train.shape)
+
+magic2 = np.fromfile(trainlabelfile, dtype=np.int32, count=1).byteswap().squeeze()
+assert(magic2 == 2049)
+Y_train_aux = np.memmap(trainlabelfile, dtype=np.dtype('>u1'), mode='r', offset=8)
+Y_train = np.zeros((10, dims[0]))
+for i in range(dims[0]):
+    Y_train[Y_train_aux[i] % 10, i] = 1
+print("Train labels:", Y_train.shape)
+
+clothes_dict = {0:"T-shirt", 1:"Trouser", 2:"Pullover", 3:"Dress", 4:"Coat",
+                5:"Sandal", 6:"Shirt", 7:"Sneaker", 8:"Bag", 9:"Ankle boot"}
+
+choice = input("See some examples? Y\\N\n")
+while (choice.lower() == "y"):
+    for i in range(5):
+        ex = np.random.randint(0, dims[0])
+        print("Example:", clothes_dict[np.argmax(Y_train[:, ex])])
+        plt.imshow(X_train[:, ex].reshape([28, 28]))
+        plt.show()
+    choice = input("See some more examples? Y\\N\n")
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Some machine learning applications built with just python and numpy.
+A repository of my machine learning applications that I built either from scratch with Pyton and Numpy or
+using more advanced deep learning libraries (pytorch).
