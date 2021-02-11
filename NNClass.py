21d4416c255141c6ca2a90e66ef7d24ebeca30ba
Mihnea Buzatu <buzatu.mihnea@yahoo.com>
2021-02-12 00:55:46 +0200

    cleaned up code

--- NNClass.py
+++ NNClass.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib
 from utils import *
 
 class NN:
@@ -29,7 +28,7 @@ class NN:
         linear_cache = (A_prev, W, b)
         A = activation(Z)
 
-        if (dropout and self.keep_prob < 1 ):
+        if dropout and self.keep_prob < 1:
             D = np.random.rand(*A.shape) < self.keep_prob
             A = (A * D) / self.keep_prob
             linear_cache = (A_prev, W, b)
@@ -39,7 +38,7 @@ class NN:
         activation_cache = Z
         cache = (linear_cache, activation_cache, D)
 
-        assert(A.shape == (W.shape[0], A_prev.shape[1]))
+        assert A.shape == (W.shape[0], A_prev.shape[1])
         return A, cache
 
     def model_fw(self, X, dropout=True):
@@ -50,26 +49,26 @@ class NN:
         for i in range(1, L):
             A_prev = A
             A, cache = self.activation_fw(A_prev, self.parameters['W' + str(i)],
-                                        self.parameters['b' + str(i)], relu, dropout)
+                                          self.parameters['b' + str(i)], relu, dropout)
             caches.append(cache)
 
         AL, cache = self.activation_fw(A, self.parameters['W' + str(L)],
-                                     self.parameters['b' + str(L)], sigmoid, False)
+                                       self.parameters['b' + str(L)], sigmoid, False)
         caches.append(cache)
 
-        assert(AL.shape == (self.layers[L], X.shape[1]))
+        assert AL.shape == (self.layers[L], X.shape[1])
         return AL, caches
 
     def compute_cost(self, AL, Y):
         m = Y.shape[1]
         cost = -(np.multiply(np.log(AL), Y) + np.multiply(np.log(1 - AL), (1 - Y))).sum()/m
-        if (self.lambd):
+        if self.lambd:
             L = len(self.layers)
             params_sum = 0
             for i in range(1, L):
                 params_sum += np.sum(np.square(self.parameters["W" + str(i)]))
             cost += self.lambd / (2 * m) * params_sum
-        assert(cost.shape == ())
+        assert cost.shape == ()
         return cost
 
     def linear_bw(self, dZ, linear_cache):
@@ -77,20 +76,20 @@ class NN:
         m = A_prev.shape[1]
 
         dW = np.dot(dZ, A_prev.T) / m
-        if (self.lambd):
+        if self.lambd:
             dW += (self.lambd / m) * W
         db = np.sum(dZ, axis=1, keepdims=True) / m
         dA_prev = np.dot(W.T, dZ)
 
-        assert (dA_prev.shape == A_prev.shape)
-        assert (dW.shape == W.shape)
-        assert (db.shape == b.shape)
+        assert dA_prev.shape == A_prev.shape
+        assert dW.shape == W.shape
+        assert db.shape == b.shape
         return dA_prev, dW, db
 
     def activation_bw(self, dA, cache, activation):
         linear_cache, activation_cache, D = cache
 
-        if (D.any()):
+        if D.any():
             dA = (dA * D) / self.keep_prob
         dZ = activation(dA, activation_cache)
         dA_prev, dW, db = self.linear_bw(dZ, linear_cache)
@@ -100,7 +99,6 @@ class NN:
     def model_bw(self, AL, Y, caches, ):
         grads = {}
         L = len(self.layers) - 1
-        m = AL.shape[1]
 
         dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
         grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = (
@@ -124,7 +122,7 @@ class NN:
             s += np.mean(self.parameters[i])
         return s
 
-    def learn(self, X, Y, iter=100, suppress=False):
+    def learn(self, X, Y, iters=100, suppress=False):
         self.normalization_u = np.mean(X, axis=1).reshape(X.shape[0], 1)
         X_norm = X - self.normalization_u
         self.normalization_sigma = (np.std(X_norm, axis=1).reshape(X.shape[0], 1) +
@@ -137,10 +135,10 @@ class NN:
         self.update_params(grads)
         cost = self.compute_cost(AL, Y)
         costs.append(cost)
-        if (not suppress):
+        if not suppress:
             print("Iters: ", 0, " Cost:", cost)
 
-        for i in range(1, iter + 1):
+        for i in range(1, iters + 1):
             AL, cache = self.model_fw(X_norm)
             grads = self.model_bw(AL, Y, cache)
             self.update_params(grads)
@@ -155,7 +153,7 @@ class NN:
         X_norm /= self.normalization_sigma
 
         AL, cache = self.model_fw(X_norm, dropout=False)
-        if (len(AL) > 1):
+        if len(AL) > 1:
             return np.argmax(AL), np.max(AL)
         return AL
 
@@ -164,7 +162,7 @@ class NN:
         count = 0
         for i in range(m):
             prediction = self.predict(X_test[:, i].reshape(X_test.shape[0], 1))[0]
-            if (prediction == Y_test[i]):
+            if prediction == Y_test[i]:
                 count += 1
         return count / m * 100
 
@@ -193,10 +191,10 @@ class NN:
         for i in range(1, len(self.layers)):
             self.parameters['W' + str(i)] = (
                 np.fromfile(file, dtype=np.float64, count=self.layers[i]*self.layers[i-1])
-                    .reshape(self.layers[i], self.layers[i-1]))
+                .reshape(self.layers[i], self.layers[i-1]))
             self.parameters['b' + str(i)] = (
                 np.fromfile(file, dtype=np.float64, count=self.layers[i])
-                    .reshape(self.layers[i], 1))
+                .reshape(self.layers[i], 1))
 
     def info(self):
         print("Layers:", self.layers)
@@ -208,7 +206,7 @@ class NN:
         print("Testing activation functions: ", x)
         print("Sigmoid:", list(sigmoid(x)))
         print("Sigmoid Grad:", list(sigmoid_der(x, np.ones(5))))
-        print("ReLu:" , list(relu(x)))
+        print("ReLu:", list(relu(x)))
         print("ReLu Grad:", list(relu_der(x, np.ones(5))))
         print("Layer sizez:", self.layers)
         print("Parameter matrix sizes")
@@ -229,7 +227,7 @@ class NN:
         print("Backward pass on 100 examples")
         grads2 = self.model_bw(AL2, np.ones(AL2.shape), cache2)
         print("Done!")
-        for key in self.parameters.keys():
+        for key in self.parameters:
             assert('d' + key in grads1 and 'd' + key in grads2)
         print("Update parameters")
         self.update_params(grads1)
