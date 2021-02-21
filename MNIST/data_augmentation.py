c6f272ba2281b0273501da9e45327c0cf15e7672
mihneabuz <mihneabuzatu88@gmail.com>
2021-02-22 00:49:52 +0200

    Deleted some imports used for testing. Whoops

--- MNIST/data_augmentation.py
+++ MNIST/data_augmentation.py
@@ -1,8 +1,6 @@
 import numpy as np
 import random
 from scipy import ndimage
-from drawimage import drawImage
-from matplotlib import pyplot as plt
 
 def random_rotate(data, degrees=10):
     results = np.zeros(data.shape)
