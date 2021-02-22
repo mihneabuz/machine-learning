99c29392280932ff6703e939beb5694231d59597
mihneabuz <mihneabuzatu88@gmail.com>
2021-02-22 02:22:01 +0200

    Done

--- MNIST/MNIST_pytorch.py
+++ MNIST/MNIST_pytorch.py
@@ -1,23 +1,26 @@
+from sys import path
+path.append('../utils')
+
 from time import time
 import torch
 from torch import nn
 import numpy as np
 import matplotlib.pyplot as plt
 from load_data import load_train, load_test
-from data_augmentation import random_rotate, mean_normalize
+from data_augmentation import random_rotate, mean_normalize, compute_mean_std
 
 print("Loading data...")
 
 X, Y, dims = load_train()
 X_test, y_test, dimst = load_test()
-dims = [dims[0], 1, dims[1], dims[2]]
-dimst = [dimst[0], 1, dimst[1], dimst[2]]
+dims = (dims[0], 1, dims[1], dims[2])
+dimst = (dimst[0], 1, dimst[1], dimst[2])
 
 X = X.reshape(dims)
 X_test = X_test.reshape(dimst)
 y_test = y_test.reshape(dimst[0])
 
-choice = input("Augment data? Y\\N\n")
+choice = input("Augment data with rotations? Y\\N\n")
 if choice.lower() == "y":
     X_rot = random_rotate(X, degrees=15)
     Y_rot = np.copy(Y)
@@ -28,8 +31,10 @@ if choice.lower() == "y":
 print("Train set:", X.shape)
 print("Train labels:", Y.shape)
 
-X = mean_normalize(X)
-X_test = mean_normalize(X_test)
+# mean normalization
+mean, std = compute_mean_std(X)
+X = mean_normalize(X, mean, std)
+X_test = mean_normalize(X_test, mean, std)
 
 # visualize some pictures
 choice = input("See some examples? Y\\N\n")
@@ -38,7 +43,7 @@ while choice.lower() == "y":
         ex = np.random.randint(0, dims[0])
         print("Example", ex, ":", Y[ex])
         plt.figure(Y[ex])
-        plt.imshow(X[ex].reshape([28, 28]), cmap="gray")
+        plt.imshow(X[ex, 0], cmap="gray")
         plt.show()
     choice = input("See some more examples? Y\\N\n")
 
@@ -59,15 +64,14 @@ train_dl = torch.utils.data.DataLoader(train_ds, batch_size=8192, shuffle=True)
 
 # model
 model = nn.Sequential(
-    nn.Conv2d(1, 4, kernel_size=5, stride=1, padding=2),
+    nn.Conv2d(1, 8, kernel_size=5, stride=1, padding=2),
     nn.MaxPool2d(kernel_size=2, stride=2),
-    nn.Conv2d(4, 10, kernel_size=3, stride=1, padding=1),
+    nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
     nn.MaxPool2d(kernel_size=2, stride=2),
     nn.Flatten(),
-    nn.Linear(7 * 7 * 10, 200),
-    nn.Linear(200, 100),
-    nn.Linear(100, 10),
-#    nn.Softmax(dim=0)
+    nn.Linear(7 * 7 * 16, 256),
+    nn.Linear(256, 128),
+    nn.Linear(128, 10),
 )
 if cuda:
     model.cuda()
@@ -78,7 +82,7 @@ criterion = nn.CrossEntropyLoss()
 # optimizer
 optim = torch.optim.AdamW(model.parameters(), lr=0.0003, weight_decay=0.2)
 
-# helper function to calculate accuracy
+# helper function to calculate accuracy on a given set
 def calculate_accuracy(X_set, y_set):
     with torch.no_grad():
         _, preds_set = torch.max(model(X_set), dim=1)
@@ -92,10 +96,14 @@ X_test = X_test.cpu()
 y_test = y_test.cpu()
 
 # training
-epochs = 5
+epochs = 50
 losses = []
 print("Training for {} epochs...".format(epochs))
 start_time = time()
+
+# memes
+if cuda:
+    print("HAha GPU goes brrrrr")
 
 for epoch in range(epochs):
     for X, y in train_dl:
@@ -143,8 +151,8 @@ if choice.lower() == 'y':
     model.cpu()
     while choice.lower() == 'y':
         from drawimage import drawImage
-        X_draw = np.array(drawImage(), dtype=np.float32) / 256.
-        X_draw = torch.from_numpy(X_draw.reshape(1, 1, dims[2], dims[3]))
+        X_draw = np.array(drawImage(), dtype=np.float32).reshape((1, 1, dims[2], dims[3]))
+        X_draw = torch.from_numpy(mean_normalize(X_draw, mean, std))
         prediction = np.argmax(model(X_draw).detach().numpy())
         print("That's a {}!".format(prediction))
         choice = input("Another one? Y/N\n")
